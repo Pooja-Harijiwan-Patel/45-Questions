e3dc6d6c6fc9b1ed5db355a4a9eb6b77b417e09a
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-17 07:08:53 -0700

    edited

--- question11.ts
+++ question11.ts
@@ -1,4 +1,4 @@
 let friends:any = ['Erica', 'Sania','Sheeza']
 console.log("1:"+friends[0])
-console.log("1:"+friends[1])
-console.log("1:"+friends[2])
+console.log("2:"+friends[1])
+console.log("3:"+friends[2])
