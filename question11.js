cfbf3a208bc6f99cdc5465a27fc07a6b86d5d163
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-17 06:50:18 -0700

    question 11

---
+++ question11.js
@@ -0,0 +1,4 @@
+var friends = ['Erica', 'Sania', 'Sheeza'];
+console.log("1:" + friends[0]);
+console.log("1:" + friends[1]);
+console.log("1:" + friends[2]);
---
+++ question11.ts
@@ -0,0 +1,4 @@
+let friends:any = ['Erica', 'Sania','Sheeza']
+console.log("1:"+friends[0])
+console.log("1:"+friends[1])
+console.log("1:"+friends[2])
