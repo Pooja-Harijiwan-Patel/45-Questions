632ca10ba2dda0ad45db148dd8580bd2f56f491c
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-17 06:17:55 -0700

    question 06

---
+++ question6.js
@@ -0,0 +1,6 @@
+var personName1 = "\t \n  John Doe \n \t";
+console.log("Name with whitespace:");
+console.log(personName1);
+var strippedName = personName1.trim();
+console.log("\nName after stripping whitespace:");
+console.log(strippedName);
---
+++ question6.ts
@@ -0,0 +1,6 @@
+let personName1: string = "\t \n  John Doe \n \t";
+console.log("Name with whitespace:");
+console.log(personName1);
+let strippedName: string = personName1.trim();
+console.log("\nName after stripping whitespace:");
+console.log(strippedName);
