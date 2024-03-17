b8143726d5122dd16ef29473cac84d89d1da3f8f
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-17 06:09:15 -0700

    question 05

---
+++ question5.js
@@ -0,0 +1,3 @@
+var famous_person = "Albert Einstein";
+var quote1 = "'Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution.'";
+console.log(famous_person + "-" + quote1);
---
+++ question5.ts
@@ -0,0 +1,3 @@
+let famous_person:string = "Albert Einstein"
+let quote1:string = "'Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution.'"
+console.log(famous_person+"-"+quote1) 
