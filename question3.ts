8d313f3f769d7ac86ae5381c6d20e1985e894c8e
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-17 05:34:27 -0700

    question 03

---
+++ question3.ts
@@ -0,0 +1,4 @@
+let full_name:string = "Eric John"
+console.log("Lower Case:", full_name.toLowerCase());
+console.log("Upper Case:", full_name.toUpperCase());
+console.log("Title Case:", full_name.charAt(0).toUpperCase()+full_name.slice(1).toLowerCase());
