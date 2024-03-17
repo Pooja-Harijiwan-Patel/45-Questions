47b67d1ba0432dc20f4b16ca6e15e9ea5584973c
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-17 06:42:39 -0700

    question 10

---
+++ question10.js
@@ -0,0 +1,12 @@
+/*Author: Pooja Harijiwan Patel
+Date: 17/3/2024
+*/
+//declare variable Fav_num and store value 30 in it
+var Fav_num = 30;
+//Now print your favourite number alongwith the message
+console.log("My favourite number is: " + Fav_num);
+//Quote from my favourite famous person
+var Famous_person = "Albert Einstein";
+//Print the quote alongwith famous person name
+var Quote1 = "'Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution.'";
+console.log(Famous_person + "-" + Quote1);
---
+++ question10.ts
@@ -0,0 +1,18 @@
+/*Author: Pooja Harijiwan Patel
+Date: 17/3/2024
+*/
+
+//declare variable Fav_num and store value 30 in it
+let Fav_num:number = 30
+
+//Now print your favourite number alongwith the message
+console.log("My favourite number is: "+Fav_num)
+
+
+//Quote from my favourite famous person
+let Famous_person:string = "Albert Einstein"
+
+//Print the quote alongwith famous person name
+let Quote1:string = "'Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution.'"
+console.log(Famous_person+"-"+Quote1) 
+
