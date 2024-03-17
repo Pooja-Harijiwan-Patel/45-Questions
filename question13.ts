1c7080a6eb1afff97556018b618eb2348beb9875
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-17 07:35:29 -0700

    question 13

---
+++ question13.ts
@@ -0,0 +1,5 @@
+let transportation: string[] = ["motorcycle", "car", "bicycle", "train", "plane"];
+
+transportation.forEach((item: string) => {
+    console.log(`I would like to own a ${item}.`);
+});
