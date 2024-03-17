a29e8076b0e7ceef520dea128ceedaceea7c70e1
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-17 06:22:46 -0700

    question 07

---
+++ question7.js
@@ -0,0 +1,8 @@
+var addition = 4 + 4;
+var subtraction = 10 - 2;
+var multiplication = 4 * 2;
+var division = 16 / 2;
+console.log("Addition: " + addition);
+console.log("subtraction: " + subtraction);
+console.log("multiplication: " + multiplication);
+console.log("Division: " + division);
---
+++ question7.ts
@@ -0,0 +1,8 @@
+let addition:number = 4+4
+let subtraction:number = 10 - 2
+let multiplication:number = 4*2
+let division:number = 16/2
+console.log("Addition: "+addition)
+console.log("subtraction: "+subtraction)
+console.log("multiplication: "+multiplication)
+console.log("Division: "+division)
