e187706b344557661669e8315e6cfbe5c1a0124f
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-17 08:13:57 -0700

    question 14

---
+++ question14.js
@@ -0,0 +1,4 @@
+var guests = ["Eric", "John", "Tim", "Min", "Rose"];
+guests.forEach(function (item) {
+    console.log("".concat(item, " I would like to invite you to party!."));
+});
---
+++ question14.ts
@@ -0,0 +1,5 @@
+let guests: string[] = ["Eric", "John", "Tim", "Min", "Rose"];
+
+guests.forEach((item: string) => {
+    console.log(`${item} I would like to invite you to party!.`);
+});
