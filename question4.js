7896ace9d60d43d5189086c96cb937df33974263
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-17 06:03:08 -0700

    question 04

---
+++ question4.js
@@ -0,0 +1,2 @@
+var quote = "'It's fine to celebrate success, but it is more important to heed the lessons of failure.'";
+console.log("Bill Gates-" + quote);
---
+++ question4.ts
@@ -0,0 +1,2 @@
+let quote:string = "'It's fine to celebrate success, but it is more important to heed the lessons of failure.'"
+console.log("Bill Gates-"+quote)
