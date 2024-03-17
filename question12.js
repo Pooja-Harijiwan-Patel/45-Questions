1e7bfacbab54f0acbc07a43dcf17c71954f4facc
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-17 07:09:27 -0700

    question 12

---
+++ question12.js
@@ -0,0 +1,5 @@
+var friends_list = ['Erica', 'Sania', 'Sheeza'];
+var greetings = "Hello! Welcome to Coding World.";
+console.log("1: " + " " + friends_list[0] + " " + greetings);
+console.log("2: " + " " + friends_list[1] + " " + greetings);
+console.log("3: " + " " + friends_list[2] + " " + greetings);
---
+++ question12.ts
@@ -0,0 +1,5 @@
+let friends_list:any = ['Erica', 'Sania','Sheeza']
+let greetings:string = "Hello! Welcome to Coding World."
+console.log("1: "+" "+friends_list[0]+" "+greetings)
+console.log("2: "+" "+friends_list[1]+" "+greetings)
+console.log("3: "+" "+friends_list[2] +" "+ greetings)
