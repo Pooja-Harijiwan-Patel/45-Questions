e0623b55e724b4e9c69bda99d4c8722d4ec16bcd
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-19 13:33:43 -0700

    question 20

---
+++ question20.js
@@ -0,0 +1,11 @@
+var mountains = [
+    "Mount Everest",
+    "K2",
+    "Kangchenjunga",
+    "Lhotse",
+    "Makalu"
+];
+console.log("My favorite mountains:");
+mountains.forEach(function (item) {
+    console.log("-> ", item);
+});
---
+++ question20.ts
@@ -0,0 +1,12 @@
+const mountains: string[] = [
+    "Mount Everest",
+    "K2",
+    "Kangchenjunga",
+    "Lhotse",
+    "Makalu"
+  ];
+  
+  console.log("My favorite mountains:");
+  mountains.forEach(function (item) {
+    console.log("-> ",item);
+});
