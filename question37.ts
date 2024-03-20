f5deaa57047b7a4c4a612b37626fc675ea4ff976
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-20 11:33:03 -0700

    modified

--- question37.ts
+++ question37.ts
@@ -1,8 +1,8 @@
-function make_shirt(size: string = "large", message: string = "I love TypeScript"): void {
+function make_shirt1(size: string = "large", message: string = "I love TypeScript"): void {
     console.log(`You ordered a ${size} t-shirt with the message: "${message}"`);
   }
   
 
-  make_shirt();
-  make_shirt("medium");
-  make_shirt("small", "I love Java too!"); 
+  make_shirt1();
+  make_shirt1("medium");
+  make_shirt1("small", "I love Java too!"); 
