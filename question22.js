07ef33c5224a58ef9f825d6debb4a4a2e7a19817
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-20 07:16:08 -0700

    question 22

---
+++ question22.js
@@ -0,0 +1,4 @@
+var flowers = ["Sunflower", "Daffodil", "Rose", "Lily"];
+//when i try to print item 4 in array flower it gives undefined in output since the index of array starts from 0
+console.log("My favourite flower is: ", flowers[4]);
+console.log("My most favourite flower is: ", flowers[3]);
