0da562a4d51405226418526d890f7d6fd6afbfb5
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-20 12:07:37 -0700

    question 41

---
+++ question41.js
@@ -0,0 +1,11 @@
+var famousMagicians = [
+    "David Copperfield",
+    "Harry Houdini",
+    "Penn Jillette",
+    "David Blaine",
+    "Criss Angel"
+];
+function magician_names() {
+    console.log(famousMagicians);
+}
+magician_names();
---
+++ question41.ts
@@ -0,0 +1,13 @@
+const famousMagicians: string[] = [
+    "David Copperfield",
+    "Harry Houdini",
+    "Penn Jillette", 
+    "David Blaine",
+    "Criss Angel"
+];
+
+function magician_names(){
+console.log(famousMagicians);
+}
+
+magician_names();
