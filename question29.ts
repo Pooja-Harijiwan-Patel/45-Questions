977e6d7e3aaf94c41218140df0c0dbbdf99e625f
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-20 09:54:07 -0700

    question 29

---
+++ question29.ts
@@ -0,0 +1,13 @@
+let fav_fruits:string[] = ["Mango","Kiwi","Strawberry"]
+
+if(fav_fruits[0]=="Mango"){
+    console.log("Mango is a fruit of summer season.")
+}else if(fav_fruits[1]===("Apple")){
+    console.log("An apple a day keeps the doctor away!")
+}else if(fav_fruits[1]=="PineApple"){
+    console.log("I like pine apple cake!")
+}else if(fav_fruits[1]=="Cherry"){
+    console.log("I love Red Cherrys")
+}else{
+    console.log("I don't eat fruits.")
+}
