f360549be0c076ca9300b6771acafd695230ec00
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-20 10:28:23 -0700

    question 36

---
+++ question36.js
@@ -0,0 +1,5 @@
+function make_shirt(size, message) {
+    console.log("You ordered a ".concat(size, " t-shirt with the message: \"").concat(message, "\""));
+}
+// Call the function with arguments
+make_shirt('large', 'Do Your Best');
---
+++ question36.ts
@@ -0,0 +1,6 @@
+function make_shirt(size: string, message: string): void {
+    console.log(`You ordered a ${size} t-shirt with the message: "${message}"`);
+  }
+  
+  // Call the function with arguments
+  make_shirt('large', 'Do Your Best');
