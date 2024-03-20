7dfad64e978731c132f7c984cf25b9c4f08198c4
Pooja-Harijiwan-Patel <pp5034748@gmail.com>
2024-03-20 09:03:54 -0700

    question 26

---
+++ question26.js
@@ -0,0 +1,16 @@
+var alienColor = "Green";
+if (alienColor == "Green") {
+    console.log("Congratulations! You earned 5 points for shooting alien.");
+}
+else {
+    console.log("Congratulation! You earned 10 points.");
+}
+//For else condition
+console.log("-----For Else Condition--------");
+var alien_Color = "Red";
+if (alien_Color == "Green") {
+    console.log("Congratulations! You earned 5 points for shooting alien.");
+}
+else {
+    console.log("Congratulation! You earned 10 points.");
+}
---
+++ question26.ts
@@ -0,0 +1,16 @@
+let alienColor:string="Green"
+if (alienColor=="Green"){
+    console.log("Congratulations! You earned 5 points for shooting alien.")
+}else{
+    console.log("Congratulation! You earned 10 points.")
+}
+
+//For else condition
+
+console.log("-----For Else Condition--------")
+let alien_Color:string="Red"
+if (alien_Color=="Green"){
+    console.log("Congratulations! You earned 5 points for shooting alien.")
+}else{
+    console.log("Congratulation! You earned 10 points.")
+}
